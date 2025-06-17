7ffd6ccc4a6412e469d44f0daaf8708a92259d67
홍명 <hongbook@hongbook.local>
2025-06-17 15:50:19 +0900

    first commit

--- templates_app/urls.py
+++ templates_app/urls.py
@@ -5,8 +5,16 @@ app_name = 'templates_app'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    # 스타트업 템플릿
+    path('startup/', views.startup_landing, name='startup_landing'),
+    # 상품소개 템플릿
+    path('product/', views.product_landing, name='product_landing'),
+    # 음식점 템플릿
+    path('restaurant/', views.restaurant_landing, name='restaurant_landing'),
+    # 교회/성당 템플릿 - 전체 사이트
     path('traditional/', views.traditional_template, name='traditional'),
     path('modern/', views.modern_template, name='modern'),
+    # 교회/성당 템플릿 - 랜딩페이지
     path('landing-modern/', views.modern_landing, name='modern_landing'),
     path('landing-traditional/', views.traditional_landing, name='traditional_landing'),
 ]
